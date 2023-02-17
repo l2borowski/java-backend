885baf2c02bcbc2454dc6283b2ac837789512cd7
Lukasz Borowski <Lukasz.Borowski@bjss.com>
2023-02-17 12:51:41 +0000

    Added 'Auth' package

---
+++ src/main/java/com/example/backend/demo/auth/AuthenticationService.java
@@ -0,0 +1,38 @@
+package com.example.backend.demo.auth;
+
+import com.example.backend.demo.appuser.AppUserRepository;
+import com.example.backend.demo.security.config.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService
+{
+    private final AppUserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request)
+    {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow();
+
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse
+                .builder()
+                .token(jwtToken)
+                .build();
+    }
+}
+
