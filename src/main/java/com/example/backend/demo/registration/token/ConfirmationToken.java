103e9bf1b56bd9edfdaea380fc70695fe5a09156
Lukasz Borowski <Lukasz.Borowski@bjss.com>
2023-02-17 12:52:39 +0000

    Added 'registration.token' package

---
+++ src/main/java/com/example/backend/demo/registration/token/ConfirmationToken.java
@@ -0,0 +1,48 @@
+package com.example.backend.demo.registration.token;
+
+import com.example.backend.demo.appuser.AppUser;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class ConfirmationToken
+{
+    @SequenceGenerator(
+            name = "confirmation_token_sequence",
+            sequenceName = "confirmation_token_sequence",
+            allocationSize = 1
+    )
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "token_sequence"
+    )
+    private Long id;
+
+    @Column(nullable = false)
+    private String token;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime expiresAt;
+
+    private LocalDateTime confirmedAt;
+
+    @ManyToOne
+    @JoinColumn(
+            nullable = false,
+            name = "app_user_id"
+    )
+    private AppUser appUser;
+}
